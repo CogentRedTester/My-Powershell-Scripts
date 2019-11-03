9b8fc75dd3d7d7e6b4859f709cbc602affc0402d
FauxRedTester <cogent.redtester@outlook.com>
2019-11-03 20:17:57 +1030

    added 3 second timeout for dns requests

--- Get-Network-Devices.ps1
+++ Get-Network-Devices.ps1
@@ -7,8 +7,11 @@ $localComputer = Get-WmiObject win32_networkadapterconfiguration `
 	| Where IPAddress -NE $null
 
 # Ping subnet to find connected devices
-$SubNet = $localComputer.IPAddress.Substring(0, $localComputer.IPAddress.lastIndexOf('.') + 1)
-
+Try {
+	$SubNet = $localComputer.IPAddress.Substring(0, $localComputer.IPAddress.lastIndexOf('.') + 1)
+} Catch {
+	$SubNet = $localComputer.IPAddress[0].Substring(0, $localComputer.IPAddress[0].lastIndexOf('.') + 1)
+}
 1..254|ForEach-Object{
     Start-Process -WindowStyle Hidden ping.exe -Argumentlist "-n 1 -l 0 -f -i 2 -w 1 -4 $SubNet$_"
 }
@@ -22,9 +25,26 @@ $Computers =(arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +',','|
 # for each found device, lookup names
 ForEach ($Computer in $Computers){
 	Try{
-		$Computer.Computername = [System.Net.Dns]::GetHostEntry($Computer.ipv4).Hostname
+		#starts an asyncronous DNS query
+		$DNSQuery = [System.Net.Dns]::GetHostEntryAsync($Computer.ipv4)
+		$AlreadyFound = 0
+		$msecs = 0.0
+		
+		#checks if the query has completed every 0.5 seconds. After 3 seconds it timeouts 
+		while ((!$AlreadyFound) -and ($msecs -lt 3000)){
+			Start-Sleep -Milliseconds 500
+			if ($DNSQuery.IsCompleted){
+				$Computer.Computername = [System.Net.Dns]::GetHostEntry($Computer.ipv4).Hostname
+				$AlreadyFound = 1
+			}
+			$msecs += 500
+		}
+		if ($msecs -eq 3000){
+			"DNS Timeout: " + $Computer.ipv4
+		}
 	}
 	Catch{
+		"error finding hostname: " + $Computer.ipv4
 	}
 }
 
