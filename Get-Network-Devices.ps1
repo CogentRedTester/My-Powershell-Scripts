feef28f6c3ef8cbed7c1be24675051d405a7fc31
FauxRedTester <cogent.redtester@outlook.com>
2019-11-03 16:15:36 +1030

    modified the script to find subnet automatically

--- Get-Network-Devices.ps1
+++ Get-Network-Devices.ps1
@@ -1,8 +1,13 @@
 # Code adapted from: https://stackoverflow.com/questions/41785413/use-powershell-to-get-device-names-and-their-ipaddress-on-a-home-network/41797841#41797841
 
+$localhost = [System.Net.Dns]::GetHostName()
+
+$localComputer = Get-WmiObject win32_networkadapterconfiguration `
+	| Select-Object -Property @{name='IPAddress';Expression={($_.IPAddress[0])}},MacAddress `
+	| Where IPAddress -NE $null
 
 # Ping subnet to find connected devices
-$Subnet = "192.168.20."
+$SubNet = $localComputer.IPAddress.Substring(0, $localComputer.IPAddress.lastIndexOf('.'))
 1..254|ForEach-Object{
     Start-Process -WindowStyle Hidden ping.exe -Argumentlist "-n 1 -l 0 -f -i 2 -w 1 -4 $SubNet$_"
 }
@@ -10,30 +15,21 @@ $Computers =(arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +',','|
   ConvertFrom-Csv -Header Computername,IPv4,MAC,x,Vendor|
                    Select Computername,IPv4,MAC
 
+
+
+""				   
+"-----------Local Network Devices-----------"
+
 # for each found device, lookup names
 ForEach ($Computer in $Computers){
 	Try{
 		$Computer.Computername = [System.Net.Dns]::GetHostEntry($Computer.ipv4).Hostname
 	}
 	Catch{
-		"Could not get Hostname for " + $Computer.ipv4
 	}
 }
 
-""				   
-"-----------Local Network Devices-----------"
 $Computers
 
-#""
-#"--------------Current Machine--------------"
-#""
-
-$localhost = [System.Net.Dns]::GetHostName()
-
-$localComputer = Get-WmiObject win32_networkadapterconfiguration `
-	| Select-Object -Property @{name='IPAddress';Expression={($_.IPAddress[0])}},MacAddress `
-	| Where IPAddress -NE $null
-
-#New-Object PsObject -Property @{ Computername = "Computername" ; IPv4 = "IPv4"; MAC = "MAC" }
 New-Object PsObject -Property @{ Computername = "------------" ; IPv4 = "--------------"; MAC = "-----------------" }
 New-Object PsObject -Property @{ Computername = $localhost ; IPv4 = $localComputer.IPAddress; MAC = $localComputer.MacAddress }
