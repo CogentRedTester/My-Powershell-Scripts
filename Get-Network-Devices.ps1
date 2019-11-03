803f35804fbd6ed8c9da15c0db1ecc5a4d4fafc5
Oscar Manglaras <cogent.redtester@outlook.com>
2019-11-03 13:37:19 +1030

    modified script to find static ip hostnames

--- Get-Network-Devices.ps1
+++ Get-Network-Devices.ps1
@@ -1,5 +1,6 @@
 # Code adapted from: https://stackoverflow.com/questions/41785413/use-powershell-to-get-device-names-and-their-ipaddress-on-a-home-network/41797841#41797841
 
+$Comuters
 
 # Ping subnet to find connected devices
 $Subnet = "192.168.20."
@@ -10,12 +11,18 @@ $Computers =(arp.exe -a | Select-String "$SubNet.*dynam") -replace ' +',','|
   ConvertFrom-Csv -Header Computername,IPv4,MAC,x,Vendor|
                    Select Computername,IPv4,MAC
 
+""				   
+"-----Local Network Devices-----"
+""
+				   
 # for each found device, lookup names
 ForEach ($Computer in $Computers){
-  nslookup $Computer.IPv4|Select-String -Pattern "^Name:\s+([^\.]+).*$"|
-    ForEach-Object{
-      $Computer.Computername = $_.Matches.Groups[1].Value
-    }
+	Try{
+		$Computer.Computername = [System.Net.Dns]::GetHostByAddress($Computer.ipv4).Hostname
+	}
+	Catch{
+		"Could not get Hostname for " + $Computer.ipv4
+	}
 }
 
 $Computers
