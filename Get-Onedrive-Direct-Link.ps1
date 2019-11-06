257298968e8353f1c641b04311afa3ec998cdede
FauxRedTester <cogent.redtester@outlook.com>
2019-11-06 18:25:31 +1030

    upgraded clipboard usage

--- Get-Onedrive-Direct-Link.ps1
+++ Get-Onedrive-Direct-Link.ps1
@@ -1,4 +1,24 @@
-$URL = Read-Host -Prompt 'Enter Onedrive URL'
+#Get-ClipboardText & Set-ClipboardText from:
+#https://www.powershellgallery.com/packages/ClipboardText/
+
+Try {
+    $clip = Get-ClipboardText
+} Catch {
+    Install-Module -Name ClipboardText -Confirm
+    $clip = Get-ClipboardText
+}
+
+#if a parameter is not entered, then ask for one
+#if there is no parameter and the clipboard contains a onedrive shortened url then uses that without asking
+if (!$args) {
+    if ($clip.contains("1drv.ms")) {
+        $URL = $clip
+    } else {
+        $URL = Read-Host -Prompt 'Enter Onedrive URL'
+    }
+} else {
+    $URL = $args[0]
+}
 
 #tries to run using powershell 5 command, if that fails tries powershell 6 workaround
 Try {
@@ -14,6 +34,8 @@ Try {
 $DirectLink = $LongURL -replace "redir", "download"
 
 $DirectLink
-$DirectLink | clip
+
+#The normal clip.exe adds a newline to the end of the string
+$DirectLink | Set-ClipboardText
 
 "Direct Link copied to clipboard"
