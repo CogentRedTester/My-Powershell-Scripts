e901c1153125ebffd1cdda747931b905a3c33a56
FauxRedTester <cogent.redtester@outlook.com>
2019-11-06 19:53:13 +1030

    Fixed empty clipboard bug, added error messages

--- Get-Onedrive-Direct-Link.ps1
+++ Get-Onedrive-Direct-Link.ps1
@@ -1,18 +1,21 @@
 #Get-ClipboardText & Set-ClipboardText from:
 #https://www.powershellgallery.com/packages/ClipboardText/
 
+#asks the user to install the module if it does not exist on the system
 Try {
     $clip = Get-ClipboardText
-} Catch {
-    Install-Module -Name ClipboardText -Confirm
+} Catch [System.Management.Automation.CommandNotFoundException] {
+    Install-Module -Name ClipboardText
     $clip = Get-ClipboardText
 }
 
 #if a parameter is not entered, then ask for one
 #if there is no parameter and the clipboard contains a onedrive shortened url then uses that without asking
 if (!$args) {
-    if ($clip.contains("1drv.ms")) {
+    if (!($null -eq $clip) -and $clip.contains("1drv.ms")) {
         $URL = $clip
+
+        "found url in clipboard: " + $clip
     } else {
         $URL = Read-Host -Prompt 'Enter Onedrive URL'
     }
@@ -23,6 +26,7 @@ if (!$args) {
 #tries to run using powershell 5 command, if that fails tries powershell 6 workaround
 Try {
     $LongURL = (Invoke-WebRequest -uri $URL -MaximumRedirection 0 -ErrorAction Ignore).Headers.Location   
+
 } Catch {
     Try {
         Invoke-WebRequest -uri $URL -MaximumRedirection 0
@@ -31,11 +35,18 @@ Try {
     }
 }
 
-$DirectLink = $LongURL -replace "redir", "download"
+#if anything went wrong with the web request then $LongURL will equal null
+#if so run error message, and if not convert the expanded link into a direct download and print to console and clipboard
+if (!$null -eq $LongURL) {
+    $DirectLink = $LongURL -replace "redir", "download"
 
-$DirectLink
+    "Direct Link: " + $DirectLink
 
-#The normal clip.exe adds a newline to the end of the string
-$DirectLink | Set-ClipboardText
+    #The normal clip.exe adds a newline to the end of the string
+    $DirectLink | Set-ClipboardText
 
-"Direct Link copied to clipboard"
+    "Direct Link copied to clipboard"
+} else {
+    ""
+    "ERROR: invalid input, could not find Direct Link"
+}
