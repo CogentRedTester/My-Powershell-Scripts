30b67c35fe91c83331a2781710f0989dde9ce4da
FauxRedTester <cogent.redtester@outlook.com>
2019-11-20 20:38:51 +1030

    removed autoupdate and added push key to exit prompt

--- youtube-dl-audio.ps1
+++ youtube-dl-audio.ps1
@@ -1,6 +1,3 @@
-youtube-dl -U
-""
-
 if (!$args) {
     $URL = Read-Host -Prompt 'Enter Audio URL'
     $URL = $URL.split(" ")
@@ -9,3 +6,8 @@ if (!$args) {
 }
 
 youtube-dl $URL -f bestaudio -i --extract-audio --audio-format mp3 -o "$home\Music\%(title)s.%(ext)s"
+
+""
+"Press any key to exit..."
+$Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown") | OUT-NULL
+$Host.UI.RawUI.FlushInputbuffer()
--- youtube-dl.ps1
+++ youtube-dl.ps1
@@ -1,6 +1,3 @@
-youtube-dl -U
-""
-
 if (!$args) {
     $URL = Read-Host -Prompt 'Enter URL'
     $URL = $URL.split(" ")
@@ -10,3 +7,8 @@ if (!$args) {
 
 #use bestvideo[width<=1920][height<=1080]+bestaudio to limit to fullHD video
 youtube-dl $URL -f bestvideo+bestaudio -i -o "$home\Videos\%(title)s.%(ext)s"
+
+""
+"Press any key to exit..."
+$Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown") | OUT-NULL
+$Host.UI.RawUI.FlushInputbuffer()
