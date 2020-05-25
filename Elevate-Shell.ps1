400881498247dfd931706c7e5e9412d784c6c513
CogentRedTester <cogent.redtester@outlook.com>
2020-05-25 11:39:54 +0930

    Elevate-Shell: merged back into a single script

--- Elevate-Shell.ps1
+++ Elevate-Shell.ps1
@@ -1,1 +1,1 @@
-Start-Process pwsh -Verb runAs -WorkingDirectory $pwd -ArgumentList "-command Elevate-Terminal.ps1"
+Start-Process pwsh -Verb runAs -WorkingDirectory $pwd -WindowStyle Hidden -ArgumentList "-command wt -d $pwd ; exit"
--- Elevate-Terminal.ps1
+++
@@ -1,2 +0,0 @@
-Start-Process wt -ArgumentList "-d $pwd"
-Stop-Process -Id $PID
